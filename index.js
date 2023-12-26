b93fe2e7b92665165ac7fa79ea777bef4ff8da2d
PrudhviMaheshDesai <mahesh@PrudhviMaheshDesais-MacBook-Air.local>
2023-12-26 19:58:30 +0530

    episode 01

---
+++ index.js
@@ -0,0 +1,17 @@
+// const root = document.getElementById("root")
+
+// const heading = document.createElement("h2")
+
+// heading.innerHTML="Hello World..."
+
+// root.appendChild(heading)
+
+
+const parent = React.createElement("div",{id:"parent"},[
+React.createElement("h1",{id:"child"},"Hello World..."),
+React.createElement("h2",{id:"child"},"Hello World...")])
+
+
+const root = ReactDOM.createRoot(document.getElementById("root"))
+
+root.render(parent)
