b3f7ad69a4239943210c2214fcd46dd4f8a9d9f6
prudhvidesai <prudhvidesai@gmail.com>
2024-01-01 05:02:44 +0530

    root

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ import ReactDOM from "react-dom"
 
 const parent = React.createElement("div",{id:"parent1"},
 [React.createElement("h1",{},"Hello React"),
-React.createElement("h2",{},"Hello React")])
+React.createElement("h2",{},"Hello React"),React.createElement("h3",{},"Hello React")])
 
 const root = ReactDOM.createRoot(document.getElementById("root"))
 
