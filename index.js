3aa8b192d7316c94a6c2735e97f18976ccb4cf5f
prudhvidesai <prudhvidesai@gmail.com>
2023-12-31 17:19:31 +0530

    episode 02

--- index.js
+++ index.js
@@ -1,17 +1,11 @@
-// const root = document.getElementById("root")
 
-// const heading = document.createElement("h2")
+import React from "react"
+import ReactDOM from "react-dom"
 
-// heading.innerHTML="Hello World..."
 
-// root.appendChild(heading)
-import React from 'react'
-import ReactDOM from 'react-dom'
-
-const parent = React.createElement("div",{id:"parent"},[
-React.createElement("h1",{id:"child"},"Hello World..."),
-React.createElement("h2",{id:"child"},"Hello World...")])
-
+const parent = React.createElement("div",{id:"parent1"},
+[React.createElement("h1",{},"Hello React"),
+React.createElement("h2",{},"Hello React")])
 
 const root = ReactDOM.createRoot(document.getElementById("root"))
 
