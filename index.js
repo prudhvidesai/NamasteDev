af0c3e1ee68ceaf1b3bd7539059781d0b0c87a66
prudhvidesai <prudhvidesai@gmail.com>
2023-12-31 17:53:02 +0530

    node modules ignored

--- index.js
+++ index.js
@@ -7,6 +7,6 @@ const parent = React.createElement("div",{id:"parent1"},
 [React.createElement("h1",{},"Hello React"),
 React.createElement("h2",{},"Hello React")])
 
-const root = ReactDOM.createRoot(document.getElementById("roo"))
+const root = ReactDOM.createRoot(document.getElementById("root"))
 
 root.render(parent)
